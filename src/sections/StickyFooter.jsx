127c8e5da375ca914fe34a0429376b064090df0d
solersol <ttpage1tk@gmail.com>
2024-07-23 23:30:47 +0300

    vfds

--- src/sections/StickyFooter.jsx
+++ src/sections/StickyFooter.jsx
@@ -10,7 +10,7 @@ const StickyFooter = () => {
         <a href="https://x.com/MyloMagic">
           <img src={img1} alt="Image 1" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
-        <a href="https://pump.fun/">
+        <a href="https://pump.fun/A1aanP7k7d7uCoPZUEa3ERnfESqERheghDLAHpECVCaJ">
           <img src={img2} alt="Image 2" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
         <a href="https://t.me/MylosMagicShow">
