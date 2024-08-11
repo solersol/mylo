87363c4dcb524bc04f4bee2496d010c6b85b9ed6
solersol <ttpage1tk@gmail.com>
2024-08-12 01:50:27 +0300

    vdf

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,10 +3,10 @@ const Hero = () => {
     <section id="home" className="w-full flex flex-col justify-center items-center min-h-screen   bg-cover bg-center">
       {/* <h1 className="flex justify-center font-comic  text-[70px] mb-0 text-[#fff]">$F1</h1> */}
       {/* <h2 className="flex justify-center font-comic  text-[22px] mb-10 mt-0 text-[#dfe2ff]">F1 MEMES</h2> */}
-      <a href="https://pump.fun/A1aanP7k7d7uCoPZUEa3ERnfESqERheghDLAHpECVCaJ"><button className="mt-6 bg-[#2e1515] text-[#fcffd3] rounded-full px-10 py-4 hover:bg-[#f0ff00] font-comic  text-[24px]">
+      <a href="https://pump.fun/"><button className="mt-6 bg-[#2e1515] text-[#fcffd3] rounded-full px-10 py-4 hover:bg-[#f0ff00] font-comic  text-[24px]">
         BUY $MYLO
       </button></a>
-      <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#fcffd3]">CA: A1aanP7k7d7uCoPZUEa3ERnfESqERheghDLAHpECVCaJ  </h1>
+      <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#fcffd3]">CA: LOADING ...  </h1>
 
     </section>
   );
